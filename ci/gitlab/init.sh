89834e7844fea9c47c231084ef32825b7e53b718
Adrien Matissart <a.matissart@qwantresearch.com>
2018-07-04 15:38:20 +0200

    gitlab-ci: Install bash before running bash

--- ci/gitlab/init.sh
+++ ci/gitlab/init.sh
@@ -2,7 +2,6 @@
 set -e
 set -x
 
-apk add --no-cache python3 py3-pip git bash
 pip3 install docker-compose pipenv
 git clone -b esTimeout https://github.com/QwantResearch/docker_mimir.git
 mv ci/gitlab/*.yml docker_mimir/
