a7596a0a0150944082358b52db6913deef2edaa2
Adrien Matissart <a.matissart@qwantresearch.com>
2018-08-08 12:17:35 +0200

    Define a test pipeline on Luxembourg

--- ci/gitlab/download_data.sh
+++ ci/gitlab/download_data.sh
@@ -2,11 +2,11 @@
 set -e
 set -x
 
-# Download addresses
-mkdir -p $ADDR_DIR
-wget http://bano.openstreetmap.fr/data/full.csv.gz -P $ADDR_DIR
-gunzip $ADDR_DIR/full.csv.gz
+# # Download addresses
+# mkdir -p $ADDR_DIR
+# wget http://bano.openstreetmap.fr/data/full.csv.gz -P $ADDR_DIR
+# gunzip $ADDR_DIR/full.csv.gz
 
 # Download osm dataset
 mkdir -p $OSM_DIR
-wget https://download.geofabrik.de/europe/france-latest.osm.pbf -P $OSM_DIR
+wget https://download.geofabrik.de/europe/luxembourg-latest.osm.pbf -P $OSM_DIR
