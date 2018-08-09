db9e14682f0cbbcdcf5652d68b5eceda62af3236
Adrien Matissart <a.matissart@qwantresearch.com>
2018-08-09 12:19:27 +0200

    gitlab-ci: Configure tester on France

--- ci/gitlab/download_data.sh
+++ ci/gitlab/download_data.sh
@@ -2,11 +2,21 @@
 set -e
 set -x
 
-# # Download addresses
-# mkdir -p $ADDR_DIR
-# wget http://bano.openstreetmap.fr/data/full.csv.gz -P $ADDR_DIR
-# gunzip $ADDR_DIR/full.csv.gz
+# Download addresses
+mkdir -p $ADDR_DIR
+BANO_FILE=$ADDR_DIR/full.csv
+if [ ! -f $BANO_FILE ]; then
+    wget http://bano.openstreetmap.fr/data/full.csv.gz -P $ADDR_DIR
+    gunzip $ADDR_DIR/full.csv.gz
+else
+    echo "No addr download: $BANO_FILE already exists"
+fi
 
 # Download osm dataset
 mkdir -p $OSM_DIR
-wget https://download.geofabrik.de/europe/luxembourg-latest.osm.pbf -O $OSM_DIR/luxembourg-latest.osm.pbf
+OSM_FILE=$OSM_DIR/france-latest.osm.pbf
+if [ ! -f $OSM_FILE ]; then
+    wget https://download.geofabrik.de/europe/france-latest.osm.pbf -O $OSM_FILE
+else
+    echo "No osm download: $OSM_FILE already exists"
+fi
